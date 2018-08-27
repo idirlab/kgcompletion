67db80582cf398ea78b0f935166c7d508c6919e4
farnaz-a <farahnaz.akrami@mavs.uta.edu>
2018-08-26 20:40:29 -0500

    Add files via upload

---
+++ ConvE/conve_fb15k-237.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 python main.py model ConvE dataset FB15k-237 \
+                                      input_drop 0.2 hidden_drop 0.3 feat_drop 0.2 \
+                                      lr 0.003 process True
