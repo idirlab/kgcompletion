ec4c278b390f71831587a86d099e03310cd01188
farnaz-a <farahnaz.akrami@mavs.uta.edu>
2018-08-27 10:51:15 -0500

    Add existing file

---
+++ OpenKE/openke_all.sh
@@ -0,0 +1,8 @@
+bash make.sh
+python example_train_transe.py
+python train_complex.py
+python train_distmult.py
+python train_transd.py
+python train_transh.py
+python train_transr.py
+python train_rescal.py
